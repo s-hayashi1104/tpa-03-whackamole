4291c39227f7e3db8594e55f90b5012e10fae686
s-hayashi1104 <shibahas.has@gmail.com>
2018-10-17 21:21:44 +0900

    <style lang="scss" scoped> ←あやしい・・・

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .whackamole {
   font-family: 'Bungee', sans-serif;
   max-width: 960px;
--- src/components/Moles.vue
+++ src/components/Moles.vue
@@ -18,7 +18,7 @@ export default {
 </script>
    
 
-<style lang="scss">
+<style lang="scss" scoped>
 .moles-container {
   margin-top: 20px;
   display: flex;
